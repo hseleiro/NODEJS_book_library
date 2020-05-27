f26a677c9aaf964cfdb99886181fa9062e8583a9
hseleiro <hseleiro@gmail.com>
2020-05-27 10:37:40 +0100

    Books create index new

--- routes/books.js
+++ routes/books.js
@@ -17,15 +17,19 @@ const upload = multer({
 // All Books Route
 router.get('/', async (req, res) => {
     let query = Book.find()
+
     if(req.query.title !== null && req.query.title !== '') {
         query = query.regex('title', new RegExp(req.query.title, 'i'))
     }
+
     if(req.query.publishedBefore !== null && req.query.publishedBefore !== '') {
         query = query.lte('publishDate', req.query.publishedBefore)
     }
+
     if(req.query.publishedAfter !== null && req.query.publishedAfter !== '') {
         query = query.gte('publishDate', req.query.publishedAfter)
     }
+
     try {
         const books = await query.exec()
         res.render('books/index', {
